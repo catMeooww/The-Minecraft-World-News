6a18e999859b1d8912f56c18cb0e2975bf0f2570
catMeooww <110690475+catMeooww@users.noreply.github.com>
2022-09-09 22:21:00 -0300

    Minecraft

---
+++ Mc_main.js
@@ -0,0 +1,12 @@
+function minecraft_menu(){
+    document.getElementById("minecraft_logo_text").style.visibility= "visible";
+    document.getElementById("meownium").src= "MEOWNIUM.jpg";
+    document.getElementById("meownium").style.width= "150px";
+    document.getElementById("meownium").style.height= "60px";
+}
+function minecraft_close_menu(){
+    document.getElementById("minecraft_logo_text").style.visibility= "hidden";
+    document.getElementById("meownium").src= "MEOWNIUM logo.jpg";
+    document.getElementById("meownium").style.width= "50px";
+    document.getElementById("meownium").style.height= "50px";
+}
