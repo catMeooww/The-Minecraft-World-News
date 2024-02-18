ebeaa232fee7cdccc65b70817b5e4a705c0ea821
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2024-02-18 19:50:39 -0300

    All  Ui Updated
    All the site Ui got an update
    2024 news comming soon

--- Mc_main.js
+++ Mc_main.js
@@ -1,12 +1,13 @@
 function minecraft_menu(){
     document.getElementById("minecraft_logo_text").style.visibility= "visible";
-    document.getElementById("meownium").src= "MEOWNIUM.jpg";
-    document.getElementById("meownium").style.width= "150px";
-    document.getElementById("meownium").style.height= "60px";
+    for(i=0;i < document.getElementsByClassName("optionp").length;i++){
+        document.getElementsByClassName("optionp").item(i).style.visibility = "visible";
+    }
+    
 }
 function minecraft_close_menu(){
     document.getElementById("minecraft_logo_text").style.visibility= "hidden";
-    document.getElementById("meownium").src= "MEOWNIUM logo.jpg";
-    document.getElementById("meownium").style.width= "50px";
-    document.getElementById("meownium").style.height= "50px";
+    for(i=0;i < document.getElementsByClassName("optionp").length;i++){
+        document.getElementsByClassName("optionp").item(i).style.visibility = "hidden";
+    }
 }
