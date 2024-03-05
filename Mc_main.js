97b824123928e166c29a96566e998b13d7aa9605
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2024-03-05 17:34:09 -0300

    Add files via upload

--- Mc_main.js
+++ Mc_main.js
@@ -1,6 +1,13 @@
+var r = 255;
+var g = 255;
+var b = 255;
 function minecraft_menu(){
+    r = Math.floor(Math.random()*255);
+    g = Math.floor(Math.random()*255);
+    b = Math.floor(Math.random()*255);
     document.getElementById("minecraft_logo_text").style.visibility= "visible";
     for(i=0;i < document.getElementsByClassName("optionp").length;i++){
+        document.getElementsByClassName("optionp").item(i).style.color = "rgb("+r+","+g+","+b+")";
         document.getElementsByClassName("optionp").item(i).style.visibility = "visible";
     }
     
